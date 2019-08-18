afa7479ed9227b0cbb46c74df7b1fba83c1344b9
Pokour <pokour@gmail.com>
2019-08-19 00:03:05 +0530

    update

--- Code.js
+++ Code.js
@@ -1,51 +1,57 @@
 // For Help goto  https://developers.google.com/apps-script/reference/spreadsheet/sheet
 // Use a function that wil be trigered when there is a request made to this Script there are doGet() and doPost() functions.
-
-function doGet(event){
-var result = {};
   
+  var sheetName = 'User';
+  var sheetId = '1tsb_MKAESDafYsi3C_lhaC0HLu_YyJ0BbxoXv9xkNRA';
+  var app = SpreadsheetApp;
+  var ss  = app.openById(sheetId);
+  var sheet  = ss.getSheetByName("Users");
+  var lastRow = sheet.getLastRow();
+  var lastColumn = sheet.getLastColumn();
+  var rows = sheet.getRange(2, 1,1,10);
+  
+  var result = {};  
   result.user = {};
-  result.role = {add1: "flat 11B",add2: "Jayadarsini Residency"};
+  result.role = {};
   result.library= [];
   result.workshop= {to:"hello", ti: "go"};
   result.courses= {};
-
-  if(event.parameter.role == "student"){
-  //result = {0 : "Student recieved"};
+ 
   
-  return ContentService.createTextOutput(JSON.stringify(result))
-    .setMimeType(ContentService.MimeType.JAVASCRIPT);
+  function makeObject(keys,values){
+    var obj = {};
+    for(var i=0 ; i< 11 ; i++){
+      obj[keys[i]] = values[i];
+    }
+    Logger.log(obj);
+    return obj;
   }
   
-  if(event.parameter.role == "collaborator"){
-  //result = {0 : "Student recieved"};
   
-  return ContentService.createTextOutput(JSON.stringify(result))
-    .setMimeType(ContentService.MimeType.JAVASCRIPT);
-  }
-  
-  if(event.parameter.role == "organisation"){
-  //result = {0 : "Student recieved"};
-  
-  return ContentService.createTextOutput(JSON.stringify(result))
-    .setMimeType(ContentService.MimeType.JAVASCRIPT);
-  }
-
-//return ContentService.createTextOutput(result);
-}
-
-// using a function to connect to the spreadsheet using predefined functions for sheets
-
-function sheettrigger(sheetName){
-  var sheetId = '1tsb_MKAESDafYsi3C_lhaC0HLu_YyJ0BbxoXv9xkNRA'
-  var app = SpreadsheetApp;
-  var spreadSheet = app.openById(sheetId);
-  var workSheet = spreadSheet.getSheetByName(sheetName);
-  var dataObj = {};
-  
-  return ;
-}
-
-function log(Argument){
-Logger.log('the answer is '+ Argument);
+  function doGet(event){
+     
+    var heading = sheet.getRange(1,1,1,10).getValues();
+    heading = [].concat.apply([],heading);
+    Logger.log(heading);
+//    
+//    Logger.log(result);
+    
+    var userdetails = rows.getValues();
+    userdetails = [].concat.apply([],userdetails);
+   result.test = makeObject(heading,userdetails);
+    
+    if(event.parameter.role == "student"){
+      return ContentService.createTextOutput(JSON.stringify(result))
+      .setMimeType(ContentService.MimeType.JAVASCRIPT);
+    }
+    
+    if(event.parameter.role == "collaborator"){
+      return ContentService.createTextOutput(JSON.stringify(result))
+      .setMimeType(ContentService.MimeType.JAVASCRIPT);
+    }
+    
+    if(event.parameter.role == "organisation"){
+      return ContentService.createTextOutput(JSON.stringify(result))
+      .setMimeType(ContentService.MimeType.JAVASCRIPT);
+    }
 }
