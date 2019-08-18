29b90e38d37811ccf19ff3f9380547739c09c501
Pokour <pokour@gmail.com>
2019-08-19 00:04:28 +0530

    done

--- Code.js
+++ Code.js
@@ -1,18 +1,18 @@
 // For Help goto  https://developers.google.com/apps-script/reference/spreadsheet/sheet
 // Use a function that wil be trigered when there is a request made to this Script there are doGet() and doPost() functions.
   
-  var sheetName = 'User';
+  var sheetName = 'Users';
   var sheetId = '1tsb_MKAESDafYsi3C_lhaC0HLu_YyJ0BbxoXv9xkNRA';
   var app = SpreadsheetApp;
   var ss  = app.openById(sheetId);
-  var sheet  = ss.getSheetByName("Users");
+  var sheet  = ss.getSheetByName(sheetName);
   var lastRow = sheet.getLastRow();
   var lastColumn = sheet.getLastColumn();
   var rows = sheet.getRange(2, 1,1,10);
   
   var result = {};  
   result.user = {};
-  result.role = {};
+  result.role = rows.getValues();
   result.library= [];
   result.workshop= {to:"hello", ti: "go"};
   result.courses= {};
@@ -28,13 +28,12 @@
   }
   
   
-  function doGet(event){
-     
+  function doGet(event){ 
     var heading = sheet.getRange(1,1,1,10).getValues();
     heading = [].concat.apply([],heading);
     Logger.log(heading);
-//    
-//    Logger.log(result);
+    
+    Logger.log(result);
     
     var userdetails = rows.getValues();
     userdetails = [].concat.apply([],userdetails);
