107d998c5b225051abf084428f3a917a9987816e
Pokour <pokour@gmail.com>
2019-08-25 13:29:42 +0530

    done

--- Code.js
+++ Code.js
@@ -1,14 +1,14 @@
 // For Help goto  https://developers.google.com/apps-script/reference/spreadsheet/sheet
 // Use a function that wil be trigered when there is a request made to this Script there are doGet() and doPost() functions.
   
-  var sheetName = 'Users';
+  var sheetName = 'users';
   var sheetId = '1tsb_MKAESDafYsi3C_lhaC0HLu_YyJ0BbxoXv9xkNRA';
   var app = SpreadsheetApp;
   var ss  = app.openById(sheetId);
   var sheet  = ss.getSheetByName(sheetName);
   var lastRow = sheet.getLastRow();
   var lastColumn = sheet.getLastColumn();
-  var rows = sheet.getRange(2, 1,1,10);
+  var rows = sheet.getRange(2, 1,1,lastColumn);
   
   var result = {};  
   result.user = {};
@@ -17,23 +17,21 @@
   result.workshop= {to:"hello", ti: "go"};
   result.courses= {};
  
-  
+  // This function takes 2 arrays and convert into object with key and value from arrays and return object
   function makeObject(keys,values){
     var obj = {};
-    for(var i=0 ; i< 11 ; i++){
+    for(var i=0 ; i<lastColumn ; i++){
       obj[keys[i]] = values[i];
     }
     Logger.log(obj);
-    return obj;
+    return obj;    
   }
   
   
-  function doGet(event){ 
-    var heading = sheet.getRange(1,1,1,10).getValues();
+  function doGet(event){
+    // This gets the data from sheet as a multi array using sheet.getRange(row, column, numRows, numColumns) 
+    var heading = sheet.getRange(1,1,1,lastColumn).getValues();
     heading = [].concat.apply([],heading);
-    Logger.log(heading);
-    
-    Logger.log(result);
     
     var userdetails = rows.getValues();
     userdetails = [].concat.apply([],userdetails);
