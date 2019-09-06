1db7a790febfc39630820c65985f487d2332efdf
Pokour <pokour@gmail.com>
2019-09-06 22:44:34 +0530

    functionality implement

--- Code.js
+++ Code.js
@@ -1,79 +1,86 @@
 // For Help goto  https://developers.google.com/apps-script/reference/spreadsheet/sheet
 // Use a function that wil be trigered when there is a request made to this Script there are doGet() and doPost() functions.
-  
-  var sheetName = 'users';
-  var sheetId = '1tsb_MKAESDafYsi3C_lhaC0HLu_YyJ0BbxoXv9xkNRA';
-  var app = SpreadsheetApp;
-  var ss  = app.openById(sheetId);
-  var sheet  = ss.getSheetByName(sheetName);
-  var lastRow = sheet.getLastRow();
-  var lastColumn = sheet.getLastColumn();
-  var rows = sheet.getRange(2, 1, 1, lastColumn);
-  
-  var result = {};  
-  result.user = {};
-  result.role = {};
-  result.library= [];
-  result.workshop= {};
-  result.courses= {};
- 
-  // This function takes 2 arrays and convert into object with key and value from arrays and return object
-  function makeObject(keys,values){
-    var obj = {};
-    for(var i=0 ; i<lastColumn ; i++){
-      obj[keys[i]] = values[i];
-    }
-    Logger.log(obj);
-    return obj;    
+
+var sheetId = '1tsb_MKAESDafYsi3C_lhaC0HLu_YyJ0BbxoXv9xkNRA';
+var app = SpreadsheetApp;
+var ss = app.openById(sheetId);
+
+var result = {};
+result.user = {};
+result.role = {};
+result.library = [];
+result.workshop = {};
+result.courses = {};
+
+// This function takes 2 arrays and convert into object with key and value from arrays and return object
+function makeObject(keys, values) {
+  var obj = {};
+  for (var i = 0; i < lastColumn; i++) {
+    obj[keys[i]] = values[i];
   }
-  
-  
-  function doGet(event){
-    // This gets the data from sheet as a multi array using sheet.getRange(row, column, numRows, numColumns) 
-    var headingMultiarray = sheet.getRange(1, 1, 1, lastColumn).getValues();
-    heading = headingMultiarray[0];
+  Logger.log(obj);
+  return obj;
+}
 
-    var userdetailsMultiarray = sheet.getRange(2, 1, 1, lastColumn).getValues();
-    userdetails = userdetailsMultiarray[0];
-    Logger.log(userdetails);
-    result.test = makeObject(heading, userdetails);
+function getRowData (pointer, targetSheet){
+  let sheet = ss.getSheetByName(targetSheet);
+  let lastRow = sheet.getLastRow();
+  let lastColumn = sheet.getLastColumn();
+  let multiArray = sheet
+    .getRange(pointer, 1, 1, lastColumn)
+    .getValues();
+  rowData = multiArray[0];
+  return rowData;
+}
+
+
+function doGet(event) {
+
+  if (event.parameter.action == "read") {
+    // role , requestStatus , userPointer , rolePointer , library
+    let userRow = event.parameter.userPointer;
+    let roleRow = event.parameter.rolePointer;
+    let libraryPointer = event.parameter.library;
+    let role = event.parameter.role;
     
-    // Converting the Library Pointer string to an array of integer
-      
-    var strVale = "130,235,342,124";
-    var strArr = strVale.split(',');
-    var intArr = [];
-    for(i=0; i < strArr.length; i++){
-      intArr[i] = parseInt( strArr[i] ).toFixed(0) ;
-      }
-      Logger.log(strArr);
-      Logger.log(intArr);
+    let userKey = 
 
-
-    if(event.parameter.action == "read"){
-      // role , requestStatus , userPointer , rolePointer , library
-
-      
-    }
-    else if(event.parameter.action == "update"){
-
-    }
-    else if(event.parameter.action == "write"){
-
-    }
-
-    if (event.parameter.role == "student") {
-      return ContentService.createTextOutput(JSON.stringify(result))
-        .setMimeType(ContentService.MimeType.JAVASCRIPT);
-    }
+    result.user = makeobject(keys, values);
     
-    if(event.parameter.role == "collaborator"){
-      return ContentService.createTextOutput(JSON.stringify(result))
+  }
+
+  else if (event.parameter.action == "update") {
+
+  }
+  else if (event.parameter.action == "write") {
+
+  }
+
+  // Converting the Library Pointer string to an array of integer      
+  var strVale = "130,235,342,124";
+  var strArr = strVale.split(',');
+  var intArr = [];
+  for (i = 0; i < strArr.length; i++) {
+    intArr[i] = parseInt(strArr[i]).toFixed(0);
+  }
+  Logger.log(strArr);
+  Logger.log(intArr);
+
+
+
+
+  if (event.parameter.role == "student") {
+    return ContentService.createTextOutput(JSON.stringify(result))
       .setMimeType(ContentService.MimeType.JAVASCRIPT);
-    }
-    
-    if(event.parameter.role == "organisation"){
-      return ContentService.createTextOutput(JSON.stringify(result))
+  }
+
+  if (event.parameter.role == "collaborator") {
+    return ContentService.createTextOutput(JSON.stringify(result))
       .setMimeType(ContentService.MimeType.JAVASCRIPT);
-    }
+  }
+
+  if (event.parameter.role == "organisation") {
+    return ContentService.createTextOutput(JSON.stringify(result))
+      .setMimeType(ContentService.MimeType.JAVASCRIPT);
+  }
 }
