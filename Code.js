2ba04da5d12ba24d7e38c0a9d046d036ca1fb356
Pokour <pokour@gmail.com>
2019-09-07 21:11:48 +0530

    done

--- Code.js
+++ Code.js
@@ -6,6 +6,7 @@ var app = SpreadsheetApp;
 var ss = app.openById(sheetId);
 
 var result = {};
+
 result.user = {};
 result.role = {};
 result.library = [];
@@ -13,42 +14,58 @@ result.workshop = {};
 result.courses = {};
 
 // This function takes 2 arrays and convert into object with key and value from arrays and return object
-function makeObject(keys, values) {
+function makeObject(keys, values, lastColumn) {
   var obj = {};
-  for (var i = 0; i < lastColumn; i++) {
+  for ( i = 0; i < lastColumn; i++) {
     obj[keys[i]] = values[i];
   }
   Logger.log(obj);
   return obj;
 }
 
-function getRowData (pointer, targetSheet){
-  let sheet = ss.getSheetByName(targetSheet);
-  // let lastRow = sheet.getLastRow();
-  let lastColumn = sheet.getLastColumn();
-  let rowHeadingMultiA = sheet
+function getRowData (pointer, targetSheet) {
+  var sheet = ss.getSheetByName(targetSheet);
+  var lastColumn = sheet.getLastColumn();
+  var rowHeadingMultiA = sheet
     .getRange(1,1,1,lastColumn)
     .getValues();
-  let heading = rowHeadingMultiA[0];
-  let rowDataMultiA = sheet
+  var heading = rowHeadingMultiA[0];
+  var rowDataMultiA = sheet
     .getRange(pointer, 1, 1, lastColumn)
     .getValues();
-  let rowData = rowDataMultiA[0];
-  return makeObject(heading,rowData);
+  var rowData = rowDataMultiA[0];
+  return makeObject(heading,rowData,lastColumn);
 }
 
+function getlibraryData(library){
+  // Converting the Library Pointer string to an array of integer      
+  var strVale = "24,37,41,68";
+  var strArr = strVale.split(',');
+  var intArr = [];
+  var multiObj = [];
+  for (i = 0; i < strArr.length; i++) {
+    intArr[i] = parseInt(strArr[i]).toFixed(0);
+  }
+  Logger.log(intArr);
+  for(i = 0; i < strArr.length; i++){
+    multiObj[i] = getRowData(intArr[i],"library");
+  }
+  return multiObj;
+  
+}
 
 function doGet(event) {
 
-  let userRow = event.parameter.userPointer;
-  let roleRow = event.parameter.rolePointer;
-  let library = event.parameter.library;
-  let role    = event.parameter.role;
+  var userRow = event.parameter.userPointer;
+  var roleRow = event.parameter.rolePointer;
+  var library = event.parameter.library;
+  var role    = event.parameter.role;
 
   if (event.parameter.action == "read") {
     // role , requestStatus , userPointer , rolePointer , library
-    result.user = getRowData(userRow,"user");
-    result.role = getRowData(roleRow,role);  
+    result.user     = getRowData(userRow,"users");
+    result.role     = getRowData(roleRow,role);
+    result.library  = getlibraryData(library);
   }
 
   else if (event.parameter.action == "update") {
@@ -57,19 +74,6 @@ function doGet(event) {
   else if (event.parameter.action == "write") {
 
   }
-
-  // Converting the Library Pointer string to an array of integer      
-  var strVale = "130,235,342,124";
-  var strArr = strVale.split(',');
-  var intArr = [];
-  for (i = 0; i < strArr.length; i++) {
-    intArr[i] = parseInt(strArr[i]).toFixed(0);
-  }
-  Logger.log(strArr);
-  Logger.log(intArr);
-
-
-
 
   if (event.parameter.role == "student") {
     return ContentService.createTextOutput(JSON.stringify(result))
