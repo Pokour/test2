3697755400af0d4d1fe2cb046c98020ddece9cd6
Pokour <pokour@gmail.com>
2019-09-07 23:54:18 +0530

    done

--- Code.js
+++ Code.js
@@ -6,12 +6,36 @@ var app = SpreadsheetApp;
 var ss = app.openById(sheetId);
 
 var result = {};
-
 result.user = {};
 result.role = {};
-result.library = [];
+result.library = {};
 result.workshop = {};
 result.courses = {};
+
+function doGet(event) {
+
+  var userRow = event.parameter.userPointer;
+  var roleRow = event.parameter.rolePointer;
+  var library = event.parameter.library;
+  var role    = event.parameter.role;
+
+  if (event.parameter.action == "read") {
+    // role , requestStatus , userPointer , rolePointer , library
+    result.user     = getRowData(userRow,"users");
+    result.role     = getRowData(roleRow,role);
+    
+  }
+
+  else if (event.parameter.action == "update") {
+
+  }
+  else if (event.parameter.action == "write") {
+
+  }
+
+  return callBack();
+
+}
 
 // This function takes 2 arrays and convert into object with key and value from arrays and return object
 function makeObject(keys, values, lastColumn) {
@@ -37,73 +61,7 @@ function getRowData (pointer, targetSheet) {
   return makeObject(heading,rowData,lastColumn);
 }
 
-function getlibraryData(library){
-  // Converting the Library Pointer string to an array of integer      
-  var strVale = "24,37,41,68";
-  var strArr = strVale.split(',');
-  var intArr = [];
-  var multiObj = {};
-  for (i = 0; i < strArr.length; i++) {
-    intArr[i] = parseInt(strArr[i]).toFixed(0);
-  }
-  Logger.log(intArr);
-
-  for(i = 0; i < strArr.length; i++){
-    setTimeout (( multiObj[i] = getRowData(intArr[i],"library")), 500);
-  }
-  return multiObj;
-}
-
-function getlibraryData(library){
-  // Converting the Library Pointer string to an array of integer      
-  var strVale = "24,37,41,68";
-  var strArr = strVale.split(',');
-  var intArr = [];
-  var multiObj = [];
-  for (i = 0; i < strArr.length; i++) {
-    intArr[i] = parseInt(strArr[i]).toFixed(0);
-  }
-  Logger.log(intArr);
-  for(i = 0; i < strArr.length; i++){
-    multiObj[i] = getRowData(intArr[i],"library");
-  }
-  return multiObj;
-  
-}
-
-function doGet(event) {
-
-  var userRow = event.parameter.userPointer;
-  var roleRow = event.parameter.rolePointer;
-  var library = event.parameter.library;
-  var role    = event.parameter.role;
-
-  if (event.parameter.action == "read") {
-    // role , requestStatus , userPointer , rolePointer , library
-    result.user     = getRowData(userRow,"users");
-    result.role     = getRowData(roleRow,role);
-    result.library  = getlibraryData(library);
-  }
-
-  else if (event.parameter.action == "update") {
-
-  }
-  else if (event.parameter.action == "write") {
-
-  }
-
-  if (event.parameter.role == "student") {
-    return ContentService.createTextOutput(JSON.stringify(result))
+function callBack (){
+  return ContentService.createTextOutput(JSON.stringify(result))
       .setMimeType(ContentService.MimeType.JAVASCRIPT);
-  }
-
-  if (event.parameter.role == "collaborator") {
-    return ContentService.createTextOutput(JSON.stringify(result))
-      .setMimeType(ContentService.MimeType.JAVASCRIPT);
-  }
-
-  if (event.parameter.role == "organisation") {
-    return ContentService.createTextOutput(JSON.stringify(result))
-      .setMimeType(ContentService.MimeType.JAVASCRIPT);
-  }
 }
