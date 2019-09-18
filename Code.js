177e6fc542b38643148dff5b4fdbfa2f5a0af5f6
Pokour <pokour@gmail.com>
2019-09-18 18:47:01 +0530

    done

--- Code.js
+++ Code.js
@@ -1,100 +1,140 @@
+/*
+author: Utsav Singh | email address | Contact Number
+Accepts GET request from WebApps using query parameters.
+1. Check for the role type: [student / collaborator / institution]
+2. updates global variables from the recieved data from query params.
+3. check for the action type from the query parameters action = [read / update / write]
+4. Logic to prform the required action on the sheet data.
+5. make an object with the return data to the WebApp using contentService()
+*/
+
+// Defining all the Global variables
+var result = { users: {}, role: {}, workshop: {}, courses: {} };
+
+// Column names for differeny roles stored as array
+var studentHeading =
+  ['add1', 'add2', 'add3', 'city', 'state', 'pincode', 'mobile', 'altmobile', 'instituteselected', 'institutelisted', 'institute',
+    'standard', 'interest1', 'interest2', 'interest3', 'dob'];
+var collaboratorHeading =
+  ['add1', 'add2', 'add3', 'city', 'state', 'pincode', 'mobile', 'altmobile', 'instituteselected', 'institutelisted', 'institute',
+    'collegeselected', 'collegelisted', 'college', 'Degree', 'startdate', 'enddate', 'interest1', 'interest2', 'interest3', 'dob'];
+var instituteHeading =
+  ['add1', 'add2', 'add3', 'city', 'state', 'pincode', 'office', 'officeemail', 'officephone', 'principal', 'principalemail',
+    'principalphone', 'poc', 'pocemail', 'pocphone'];
+var userHeaading = [];
+
+var userRow = 0;
+var roleRow = 0;
+var roleRecieved;
+var actionRequested;
+var library;
+
 var sheetId = '1tsb_MKAESDafYsi3C_lhaC0HLu_YyJ0BbxoXv9xkNRA';
 var app = SpreadsheetApp;
 var ss = app.openById(sheetId);
+var sheet = ss.getSheetByName("users");
 
-var result = {};
-result.user = {};
-result.role = {};
-result.library = {};
-result.workshop = {};
-result.courses = {};
+var heading = [];
+var firstColumn;
+var headLength;
 
+//********************************************* doGet() STARTS HERE ********************************************* 
 function doGet(event) {
-  // Query parameter data to function variables
-  var userRow = event.parameter.userPointer;
-  var roleRow = event.parameter.rolePointer;
-  var library = event.parameter.library;
-  var role = event.parameter.role;
-  var firstColumn = 3;
-  // Declare role headings to extract the query parameters
-  // store param data into newDataArray[]
-  // change columnLength according to the role
-  var studentHeading = ['add1', 'add2', 'add3', 'city', 'state', 'pincode', 'mobile', 'altmobile', 'instituteselected', 'institutelisted', 'institute', 'standard', 'interest1', 'interest2', 'interest3', 'dob'];
-  var collaboratorHeading = ['add1', 'add2', 'add3', 'city', 'state', 'pincode', 'mobile', 'altmobile', 'instituteselected', 'institutelisted', 'institute', 'collegeselected', 'collegelisted', 'college', 'Degree', 'startdate', 'enddate', 'interest1', 'interest2', 'interest3', 'dob'];
-  var instituteHeading = ['add1', 'add2', 'add3', 'city', 'state', 'pincode', 'office', 'officeemail', 'officephone', 'principal', 'principalemail', 'principalphone', 'poc', 'pocemail', 'pocphone'];
 
-  // Variable to store the data recieved from the query param to the array newDataArray[]
-  // Update the columnLength according to the role
-  var newDataArray = [];
-  var columnLength = 1;
-  var heading = [];
-  var data = [newDataArray];
-  var sheet = ss.getSheetByName(role);
+  updateParameters(event);
 
-  if (role == 'student') {
-    columnNames = studentHeading;
-    columnLength = 16;
-  } else if (role == 'collaborator') {
-    columnNames = collaboratorHeading;
-    columnLength = 16;
-  } else if (role == 'institute') {
-    columnNames = instituteHeading;
-    columnLength = 16;
-  }
+  updateRoleParametrs(roleRecieved);
 
-  switch (event.parameter.action){
-    case "read":{
-      break;
-    }
-    case "update":{
-      break;
-    }
-    case "write":{
-      break;
-    }
-  }
+  takeAction(event);
 
-  if (event.parameter.action == "read") {
-    // role , requestStatus , userPointer , rolePointer , library
-    result.user = getRowData(userRow,firstColumn,columnLength,columnNames, "users");
-    result.role = getRowData(roleRow,firstColumn,columnLength,columnNames, role);
-    return callBack();
-  }
+  return callBack();
 
-  else if (event.parameter.action == "update") {
+}
+//********************************************* doGet() ENDS HERE ********************************************* 
 
-    // Looping the newDataArray to get the the param data into the array using the key from heading
-    for (i = 0; i < heading.length; i++) {
-      newDataArray[i] = event.parameters[heading[i]];
-    }
+function updateParameters(event) {
+  userRow = event.parameter.userPointer;
+  roleRow = event.parameter.rolePointer;
+  roleRecieved = event.parameter.role;
+  actionRequested = event.parameter.action;
+  library = event.parameter.library;
+}
 
-    // Append the data to the sheet row    
-    sheet.getRange(roleRow, firstColumn, 1, columnLength).setValues(data);
-
-    // This will generate a data set to be returned to the request start point confirming the update process execution state
-    // Generate a metadata confirming te update operation.
-
-  }
-  else if (event.parameter.action == "write") {
-
+function updateRoleParametrs(roleRecieved) {
+  if (roleRecieved == "student") {
+    heading = studentHeading;
+    firstColumn = 3;
+    headLength = heading.length;
+  } else if (roleRecieved == "collaborator") {
+    heading = collaboratorHeading;
+    firstColumn = 3;
+    headLength = heading.length;
+  } else if (roleRecieved == "institute") {
+    heading = instituteHeading;
+    firstColumn = 3;
+    headLength = heading.length;
   }
 }
 
-function getRowData(pointer,firstColumn,columnLength,columnNames, targetSheet) {
-  var sheet = ss.getSheetByName(targetSheet);
-  var rowDataMultiA = sheet.getRange(pointer, firstColumn, 1, columnLength).getValues();
-  var rowData = rowDataMultiA[0];
-  return makeObject(columnNames, rowData, lastColumn);
+function takeAction(event) {
+  if (actionRequested == "read") {
+    readFromSheet();
+  } else if (actionRequested == "update") {
+    updateSheet(event);
+  } else if (actionRequested == "write") {
+    writeToSheet(event);
+    Logger.log("WRITE ACTION")
+  }
+}
+
+function readFromSheet() {
+  result.users = getRowData(userRow, "users");
+  result.role = getRowData(roleRow, roleRecieved);
+}
+
+function getRowData(pointer, targetSheet) {
+  sheet = ss.getSheetByName(targetSheet);
+  var tempMultiArray = sheet.getRange(pointer, firstColumn, 1, headLength).getValues();
+  var simpleArray = tempMultiArray[0];
+  return makeObject(heading, simpleArray);
 }
 
 // This function takes 2 arrays and convert into object with key and value from arrays and return object
-function makeObject(keys, values, lastColumn) {
+function makeObject(keys, values) {
   var obj = {};
-  for (i = 0; i < lastColumn; i++) {
+  for (i = 0; i < headLength; i++) {
     obj[keys[i]] = values[i];
   }
   Logger.log(obj);
   return obj;
+}
+
+function updateSheet(event) {
+  var temp = [];
+  var dataarray = [[]];
+  for (i = 0; i < headLength; i++) {
+    temp[i] = event.parameter[heading[i]];
+  }
+  dataarray[0] = temp;
+  sheet = ss.getSheetByName(roleRecieved);
+  sheet.getRange(roleRow, firstColumn, 1, headLength).setValues(dataarray);
+}
+
+function writeToSheet(event) {
+  var ulength = userHeaading.length;
+  var temp=[];
+  var dataarray=[[]];
+  for(i=0 ; i < ulength ; i++ ) {
+    temp[i] = event.parameter[userHeaading[i]];
+  }
+  dataarray[0] = temp;
+  // get last row users
+  // get last row role sheet
+  
+  sheet = ss.getSheetByName("users");
+  // Get last row | update first column etc
+  sheet.getRange(roleRow, firstColumn, 1, ulength).setValues(dataarray);
+  // return success after row append
 }
 
 function callBack() {
